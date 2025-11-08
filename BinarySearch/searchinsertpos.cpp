4d45828c2e67f7438fdfb33c99181287b14a4f62
ayushi <ayushinagar069@gmail.com>
2025-11-08 17:38:39 +0530

    searchinsert

---
+++ BinarySearch/searchinsertpos.cpp
@@ -0,0 +1,27 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size(); 
+        int low = 0, high = n - 1;
+        int ans = n;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            // maybe an answer
+            if (nums[mid] >= target) {
+                ans = mid;
+                // look for smaller index on the left
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1; // look on the right
+            }
+        }
+        return ans;
+    }
+};
