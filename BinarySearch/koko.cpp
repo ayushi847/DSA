303df3b1de3883f944d405c34d6b40de6507d6e2
ayushi <ayushinagar069@gmail.com>
2025-11-11 20:16:30 +0530

    piles

---
+++ BinarySearch/koko.cpp
@@ -0,0 +1,34 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long calculateTotalHours(vector<int>& piles, int speed) {
+        long long totalH = 0;
+        for (int bananas : piles) {
+            totalH += (bananas + speed - 1) / speed; 
+        }
+        return totalH;
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h)  {
+        int maxPile = *max_element(piles.begin(), piles.end());
+        int low = 1, high = maxPile;
+        int ans = maxPile;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            long long totalH = calculateTotalHours(piles, mid);
+
+            if (totalH <= h) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
